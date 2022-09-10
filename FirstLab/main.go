b5025ffa8da2850e9577784edc6d7e66629dd420
diyliv <108467057+diyliv@users.noreply.github.com>
2022-09-10 12:45:51 +0300

    Add files via upload

--- FirstLab/main.go
+++ FirstLab/main.go
@@ -1,11 +1,52 @@
 package main
 
 import (
+	"C"
 	"fmt"
 	"math/rand"
 	"time"
 	"unsafe"
 )
+
+/*
+// create array
+Array *array_create(size_t size)
+{
+    return new Array(size);
+}
+
+// delete array, free memory
+void array_delete(Array *arr)
+{
+    delete arr;
+}
+
+// returns specified array element
+Data array_get(const Array *arr, size_t index)
+{
+   // return (Data)0;
+    if (index < 0 || index >= array_size(arr))
+        return 0;
+    else
+        return arr->_arr[index];
+}
+
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value)
+{
+    if (index < 0 || index >= array_size(arr))
+        return;
+    else
+        arr->_arr[index] = value;
+}
+
+// returns array size
+size_t array_size(const Array *arr)
+{
+    //return 0;
+    return arr->_n;
+}
+*/
 
 type Resp struct {
 	FirstSpeed  int
@@ -55,12 +96,12 @@ func main() {
 	fmt.Printf("Enter Length Of The Array: ")
 	fmt.Scanln(&length)
 
-	arr := array_create(uint32(length), func(p unsafe.Pointer) {})
+	arr := C.array_create(uint32(length), func(p unsafe.Pointer) {})
 
 	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < length; i++ {
-		array_set(arr, uint32(i), rand.Intn(100))
+		C.array_set(arr, uint32(i), rand.Intn(100))
 	}
 	fmt.Println(arr)
 	FoundTheFastest(arr)
